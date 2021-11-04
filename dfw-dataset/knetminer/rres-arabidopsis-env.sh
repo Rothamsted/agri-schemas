8588567f1179b3ef57bdb36e49ad1e98b760b8c8
Marco Brandizi <marco.brandizi@gmail.com>
2021-11-04 19:07:28 +0100

    Adding Arabidopsis to the knetminer cases.

--- dfw-dataset/knetminer/rres-arabidopsis-env.sh
+++ dfw-dataset/knetminer/rres-arabidopsis-env.sh
@@ -1,8 +1,7 @@
 # Manages the arabidospis dataset
 # TODO: we need to introduce dataset parameters, like the rres build pipeline 
 
-# Use this prolog to go to the script's directory
 cd "`dirname ${BASH_SOURCE[0]}`"
-
 . ./rres-env.sh
+
 export KNET_DATASET_DIR="$KNET_DATA_DIR/pub/endpoints/arabidopsis/51"
