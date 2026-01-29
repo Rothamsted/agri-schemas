2cd75b4fb13d2dff49b349ce122628b409537c95
Marco Brandizi <marco.brandizi@gmail.com>
2026-01-29 19:33:31 +0000

    feat (data builds): add environment for aws-virtuoso

---
+++ dataset-builds/environments/aws-virt-update-only.sh
@@ -0,0 +1,5 @@
+# This environement is to be used on virtuoso-dev only to update the Virtuoso instance from
+# already existing data dumps, via virtuoso-upload.sh 
+#
+export RDFUTILS_HOME="/opt/software/rdfutils"
+export ETL_OUTPUT="/opt/data/dumps/agrischemas"
